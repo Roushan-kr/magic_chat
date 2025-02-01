90366e48c27ebef11957cd90d9ac3ad3f9e98c38
Roushan <kumar7roushanjha2005@gmail.com>
2025-02-01 13:19:37 +0530

    Add message suggestion and sending API endpoints with OpenAI integration

---
+++ src/app/api/(msg)/sand-msg/route.ts
@@ -0,0 +1,64 @@
+import dbConnect from "@/lib/dbconnect";
+import userModel,{Message} from "@/models/User";
+import { msgSchema } from "@/schemas/messageSchema";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  await dbConnect();
+
+  const { username, content } = await req.json();
+  try {
+    const msg = msgSchema.safeParse(content);
+    // handle the parsed message
+
+    if (!msg.success) {
+      return NextResponse.json(
+        {
+          message: "unable to parse content",
+          success: false,
+        },
+        { status: 401 }
+      );
+    }
+    const user = await userModel.findOne({ uname: username });
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "unable to get user by username",
+          success: false,
+        },
+        { status: 404 }
+      );
+    }
+    if(!user.isAcceptingMessage){
+        return NextResponse.json(
+            {
+              message: "user not accepting msg",
+              success: true,
+            },
+            { status: 403 }
+          );
+    }
+    
+    user.messages.push({content:msg.data.content , createdAt:new Date()} as Message);
+    
+    await user.save();
+    return NextResponse.json(
+        {
+          message: "msg added success",
+          success: true,
+        },
+        { status: 200 }
+      );
+   
+   
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message || "somting went worng",
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/(msg)/suggest-msg/route.ts
@@ -0,0 +1,32 @@
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
+import { NextResponse } from "next/server";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  try {
+    const { messages } = await req.json();
+
+    const prompt = `${messages} , now create a list of string response seprated by ',' using the above text like a suggesation with that context as user text response within 5 to 10 words aslo remeber it is response that a user feeback is given to another user reaction/work ensure user get best of what they write `;
+
+    const result = streamText({
+      model: openai("gpt-4o"),
+      maxTokens: 300,
+      prompt,
+    });
+
+    result.toDataStreamResponse();
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+        success: false,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
