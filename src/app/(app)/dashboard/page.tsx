89f0a010e87693ca94a4899258e2bcce534504a1
Roushan <kumar7roushanjha2005@gmail.com>
2025-02-02 16:09:04 +0530

    Add navigation bar with user greeting and authentication links in dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { User } from "next-auth";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 function page() {
+  const { data: session } = useSession();
+  const user: User = session?.user;
+
   return (
-    <div>
-      dashboard
+    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+    <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+        True Feedback
+      </a>
+      {session ? (
+        <>
+          <span className="mr-4">
+            Welcome, {user.uname || user.email}
+          </span>
+          <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
+            Logout
+          </Button>
+        </>
+      ) : (
+        <Link href="/sign-in">
+          <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
+        </Link>
+      )}
     </div>
-  )
+  </nav>
+  );
 }
 
-export default page
+export default page;
